3f2b812bd74b4132f59d36bb14d17a33ac4ade8e
t-jimbo <jbear.um@gmail.com>
2024-12-17 18:13:36 +0900

    feat: solved

--- typical90/src/14_sing_together.rs
+++ typical90/src/14_sing_together.rs
@@ -3,7 +3,20 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a_n: [usize; n],
-        b_n: [usize; n],
+        mut a_n: [usize; n], // 小学生の家の位置
+        mut b_n: [usize; n], // 小学校の位置
     }
+
+    // sort
+    a_n.sort();
+    b_n.sort();
+
+    // 端からマッチングしていけばOK
+    let mut res = 0;
+    for i in 0..n {
+        res += a_n[i].abs_diff(b_n[i]);
+    }
+
+    std::println!("{}", res);
 }
+
