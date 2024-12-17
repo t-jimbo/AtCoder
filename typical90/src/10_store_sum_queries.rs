a4bb27eefbdf957d147e3d9434148e34396c674a
t-jimbo <jbear.um@gmail.com>
2024-12-17 14:55:40 +0900

    feat: solve foolishly

--- typical90/src/10_store_sum_queries.rs
+++ typical90/src/10_store_sum_queries.rs
@@ -3,19 +3,27 @@ use proconio::input;
 fn main() {
     input! {
         n: usize, // 生徒数
-        cp: [[usize; 2]; n], // クラスと得点
+        cp: [(usize, usize); n], // クラスと得点
         q: usize, // question数
-        lr: [[usize; 2]; q], // 学籍番号の両端
+        lr: [(usize, usize); q], // 学籍番号の両端
     }
 
-    std::println!("test");
-    std::println!("{}", n);
-
-    for i in 0..n {
-        std::println!("{}, {}", cp[i][0], cp[i][1]);
-    }
-
+    // questionを処理していく
     for i in 0..q {
-        std::println!("{}, {}", lr[i][0], lr[i][1]);
+        let mut sum1 = 0;
+        let mut sum2 = 0;
+        let (l, r) = lr[i];
+
+        // 得点を合計
+        for j in (l - 1)..r {
+            let (c, p) = cp[j];
+            if c == 1 {
+                sum1 += p;
+            } else {
+                sum2 += p;
+            }
+        }
+
+        std::println!("{} {}", sum1, sum2);
     }
 }
