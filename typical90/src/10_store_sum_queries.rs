9d3404cbd7ff4b7fded41512a1f4e816ea04bcf2
t-jimbo <jbear.um@gmail.com>
2024-12-17 15:16:11 +0900

    feat: solved

--- typical90/src/10_store_sum_queries.rs
+++ typical90/src/10_store_sum_queries.rs
@@ -8,22 +8,27 @@ fn main() {
         lr: [(usize, usize); q], // 学籍番号の両端
     }
 
-    // questionを処理していく
-    for i in 0..q {
-        let mut sum1 = 0;
-        let mut sum2 = 0;
-        let (l, r) = lr[i];
+    // #0~nまでのclass1, 2の得点合計値
+    let mut sums: Vec<(usize, usize)> = Vec::with_capacity(n + 1);
+    sums.push((0, 0));
 
-        // 得点を合計
-        for j in (l - 1)..r {
-            let (c, p) = cp[j];
-            if c == 1 {
-                sum1 += p;
-            } else {
-                sum2 += p;
-            }
+    // sumsを前処理
+    for i in 0..n {
+        let (c, p) = cp[i];
+        let (prev_1, prev_2) = if i == 0 { (0, 0) } else { sums[i] };
+        if c == 1 {
+            sums.push((prev_1 + p, prev_2));
+        } else {
+            sums.push((prev_1, prev_2 + p));
         }
+    }
 
-        std::println!("{} {}", sum1, sum2);
+    // SUM(lr) = SUM(r) - SUM(l)に分解して考える
+    for i in 0..q {
+        let (l, r) = lr[i];
+        let (sum_l1, sum_l2) = sums[l - 1];
+        let (sum_r1, sum_r2) = sums[r];
+
+        std::println!("{} {}", sum_r1 - sum_l1, sum_r2 - sum_l2);
     }
 }
