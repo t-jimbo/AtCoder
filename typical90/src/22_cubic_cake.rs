3ef9361c7690d994a95f19f314a30845bc19745b
t-jimbo <jbear.um@gmail.com>
2024-12-17 15:38:31 +0900

    feat: solved but was wrong

--- typical90/src/22_cubic_cake.rs
+++ typical90/src/22_cubic_cake.rs
@@ -2,10 +2,29 @@ use proconio::input;
 
 fn main() {
     input! {
-        a: usize,
-        b: usize,
-        c: usize,
+        w: usize,
+        d: usize,
+        h: usize,
     }
 
-    std::println!("{}, {}, {}", a, b, c)
+    std::println!("{}", cut(w, d, h));
+}
+
+fn cut(w: usize, d: usize, h: usize) -> usize {
+    if w == d && d == h {
+        return 0;
+    }
+
+    if w == d && h % d == 0 {
+        return h / d - 1;
+    }
+    if d == h && w % h == 0 {
+        return w / h - 1;
+    }
+    if h == w && d % w == 0 {
+        return d / w - 1;
+    }
+
+    // 全カット
+    w + d + h - 3
 }
