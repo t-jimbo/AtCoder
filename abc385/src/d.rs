a3d821d497af78706bcdab391044a23c1a747d52
t-jimbo <jbear.um@gmail.com>
2024-12-22 12:44:20 +0900

    chore: init d

---
+++ abc385/src/d.rs
@@ -0,0 +1,12 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        sx: usize,
+        sy: usize,
+        xy: [(usize, usize); n],
+        dc: [(usize, usize): m],
+    }
+}
