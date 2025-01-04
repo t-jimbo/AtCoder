18443fa8b20db3b966eb2db7f8ccb82fa96f4784
t-jimbo <jbear.um@gmail.com>
2025-01-04 21:06:17 +0900

    solved b

--- abc387/src/b.rs
+++ abc387/src/b.rs
@@ -2,8 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        x: usize,
     }
-    
-    println!("Hello, world!");
+
+    let mut count = 0;
+
+    for i in 0..9 {
+        for j in 0..9 {
+            let sum = (i + 1) * (j + 1);
+            if sum != x {
+                count += sum;
+            }
+        }
+    }
+
+    println!("{}", count);
 }
