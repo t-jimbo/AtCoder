9b97f34fb060eab314fcb21283b85f4693ee7051
t-jimbo <jbear.um@gmail.com>
2025-01-06 14:39:36 +0900

    tmp d

--- abc385/src/d.rs
+++ abc385/src/d.rs
@@ -1,20 +1,21 @@
 use proconio::input;
 use std::collections::HashMap;
 
+type Houses = HashMap<isize, Vec<isize>>;
 
 fn main() {
     input! {
         n: usize,
         m: usize,
-        mut sx: usize, // サンタの座標
-        mut sy: usize,
-        xy: [(usize, usize); n], // 家
-        dc: [(char, usize); m], // cmd
+        mut sx: isize, // サンタの座標
+        mut sy: isize,
+        xy: [(isize, isize); n], // 家
+        dc: [(char, isize); m], // cmd
     }
 
-    let mut x_houses: HashMap<usize, Vec<usize>> = HashMap::new();
-    let mut y_houses: HashMap<usize, Vec<usize>> = HashMap::new();
-    let mut counts: HashMap<(usize, usize), bool> = HashMap::new();
+    let mut x_houses: Houses = HashMap::new();
+    let mut y_houses: Houses = HashMap::new();
+    let mut count = 0;
 
     // 前処理
     for (x, y) in xy.iter() {
@@ -38,23 +39,47 @@ fn main() {
 
     for (d, c) in dc.iter() {
         match d {
-            'U' => {}
-            'D' => {}
-            'L' => {}
-            'R' => {}
+            'U' => {
+                count += count_houses(&mut x_houses, &sx, sy + 1, sy + c);
+                sy += c;
+            }
+            'D' => {
+                count += count_houses(&mut x_houses, &sx, sy - c, sy - 1);
+                sy -= c;
+            }
+            'L' => {
+                count += count_houses(&mut y_houses, &sy, sx - c, sx - 1);
+                sx -= c;
+            }
+            'R' => {
+                count += count_houses(&mut y_houses, &sy, sx + 1, sy + c);
+                sx += c;
+            }
             _ => {
                 panic!("unexpected cmd")
             }
         }
     }
+
+    println!("{} {} {}", sx, sy, count);
 }
 
-fn mv_santa() {
-    // サンタを移動させて座標をupdateする
-    // 新しい家を通過したらcount up
-    // 家の通過の判定が難しそう。
-    //  ex: x, y -> x + c, y に動くとき。
-    //      y = y, x < x <= x + c に家があるならその座標を取得する。
+fn count_houses(houses: &mut Houses, base: &isize, l: isize, r: isize) -> usize {
+    let mut count: usize = 0;
+    match houses.get_mut(base) {
+        Some(v) => {
+            let v_read = v.clone();
+            for i in 0..v.len() {
+                let h = v_read[i];
+                if l <= h && h <= r {
+                    // 移動する範囲内に家がある場合
+                    println!("house: {}, {}", base, h);
+                    count += 1;
+                    v.remove(i); // 訪問済みの場合popする
+                }
+            }
+        }
+        None => {} // 家を通過しない
+    };
+    count
 }
-
-fn get_houses() {}
