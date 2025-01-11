bea40b4d0a87cda8b7b35384091efadb6e8db7fd
t-jimbo <jbear.um@gmail.com>
2025-01-11 22:11:06 +0900

    fix: binary search c

--- abc388/src/c.rs
+++ abc388/src/c.rs
@@ -1,4 +1,5 @@
 use proconio::input;
+use std::cmp::{max, min};
 
 fn main() {
     input! {
@@ -10,18 +11,33 @@ fn main() {
 
     // 上のモチ
     for i in 0..n {
-        if a[i] * 2 > a[n - 1] {
-            // 一番大きいやつの半分より大きかったらもうない
-            break;
-        }
+        // できない
+        let upper_mochi = a[i];
+        if upper_mochi * 2 > a[n - 1] { break; }
 
-        // 下のモチ
-        for j in (i + 1)..n {
-            if a[i] * 2 <= a[j] {
-                count += 1;
+        let mut l = i + 1;
+        let mut r = n - 1;
+        loop {
+            let mid = (l + r) / 2;
+            if l == r {
+                break;
+            }
+
+            if is_kagamimochable(upper_mochi, a[mid]) {
+                r = max(mid - 1, l);
+                if l + 1 == r {}
+            } else {
+                l = min(mid + 1, r);
             }
         }
+
+        let max_ng = if is_kagamimochable(upper_mochi, a[l]) { l } else { l + 1 };
+        count += n - max_ng;
     }
 
     println!("{}", count);
+}
+
+fn is_kagamimochable(a: usize, b: usize) -> bool {
+    a * 2 <= b
 }
