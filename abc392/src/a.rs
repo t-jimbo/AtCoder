8094399ae0b5c9a089839c259d7d6fa0d166af6a
t-jimbo <jbear.um@gmail.com>
2025-02-08 21:03:49 +0900

    a

---
+++ abc392/src/a.rs
@@ -0,0 +1,15 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a1: usize,
+        a2: usize,
+        a3: usize,
+    }
+
+    if a1 * a2 == a3 || a2 * a3 == a1 || a3 * a1 == a2 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
