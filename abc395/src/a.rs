ad76b5c5a9d19154aff2f400b948115f255628e5
t-jimbo <jbear.um@gmail.com>
2025-03-09 10:07:00 +0900

    a

--- abc395/src/a.rs
+++ abc395/src/a.rs
@@ -6,24 +6,17 @@ fn main() {
         a: [usize; n],
     }
 
-    let mut count = 0;
+    let mut flag = true;
     for i in 0..n {
         if i == 0 {
-            count += 1;
             continue;
         }
 
-        if a[i] == a[i - 1] {
-            count += 1;
-        } else {
-            count = 1;
-        }
-
-        if count >= 3 {
-            println!("Yes");
-            return;
+        if a[i] <= a[i - 1] {
+            flag = false;
+            break;
         }
     }
 
-    println!("No");
+    println!("{}", if flag { "Yes" } else { "No" });
 }
