1008123d690557abe17fa100a774f64927b5004c
t-jimbo <jbear.um@gmail.com>
2025-03-09 17:38:19 +0900

    d

--- abc391/src/d.rs
+++ abc391/src/d.rs
@@ -1,42 +1,56 @@
 use proconio::input;
-use std::cmp::min;
-use std::collections::HashSet;
-use std::collections::VecDeque;
+use std::cmp::{max, min};
 
 fn main() {
     input! {
         n: usize,
-        m: usize,
-        uvw: [(usize, usize, usize); m],
+        w: usize,
+        xy: [(usize, usize); n],
+        q: usize,
+        ta: [(usize, usize); q],
     }
 
-    let mut graph = vec![Vec::new(); n];
-    for (u, v, w) in uvw {
-        graph[u - 1].push((v - 1, w));
-        graph[v - 1].push((u - 1, w));
+    // (a, y)
+    let mut columns: Vec<Vec<(usize, usize)>> = vec![vec![]; w];
+    for i in 0..n {
+        let (x, y) = xy[i];
+        columns[x - 1].push((i, y - 1));
+    }
+    for column in columns.iter_mut() {
+        column.sort_by(|a, b| a.1.cmp(&b.1));
     }
 
-    let mut stack: VecDeque<(usize, usize, HashSet<usize>)> = VecDeque::new();
-    stack.push_back((0, 0, HashSet::new()));
+    let mut del_layer = usize::MAX; // max layer to be deleted
+    let mut a_layers: Vec<usize> = vec![0; n]; // layer block A is in
+    let mut layer_highest: Vec<usize> = vec![0; n]; // highest block y in each layer
 
-    let mut min_w = None;
+    for i in 0..w {
+        let cols = columns[i].len();
+        // find the min layer
+        del_layer = min(del_layer, cols);
 
-    while let Some((node, total_w, mut visited)) = stack.pop_front() {
-        for &(v, w) in &graph[node] {
-            if visited.contains(&v) {
-                continue;
-            }
-            if v == n - 1 {
-                if min_w.is_none() {
-                    min_w = Some(total_w ^ w);
-                } else {
-                    min_w = Some(min(min_w.unwrap(), total_w ^ w));
-                }
-            }
-            visited.insert(node);
-            stack.push_back((v, total_w ^ w, visited.clone()));
+        for j in 0..cols {
+            let (a, y) = columns[i][j];
+            a_layers[a] = j;
+            layer_highest[j] = max(layer_highest[j], y);
         }
     }
 
-    println!("{}", min_w.unwrap());
+    for i in 0..q {
+        let (t, a) = ta[i];
+        let layer = a_layers[a - 1];
+
+        // never deleted
+        if layer >= del_layer {
+            println!("Yes");
+            continue;
+        }
+
+        let del_time = layer_highest[layer] + 1;
+        if t < del_time {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }
