d7f4c8ecb26ce912d9db2bca84369126b32cc32d
t-jimbo <jbear.um@gmail.com>
2025-03-09 12:24:42 +0900

    b

--- abc394/src/b.rs
+++ abc394/src/b.rs
@@ -3,84 +3,10 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
+        mut s: [String; n],
     }
 
-    let m = n / 2;
-    for i in 0..n {
-        for j in 0..n {
-            if i < m {
-                // top half
-                if j < m {
-                    // left half
-                    if j < i {
-                        // outer
-                        if j % 2 == 0 {
-                            print!("#")
-                        } else {
-                            print!(".")
-                        }
-                    } else {
-                        // inner
-                        if i % 2 == 0 {
-                            print!("#")
-                        } else {
-                            print!(".")
-                        }
-                    }
-                } else {
-                    // right half
-                    if n - j <= i + 1 {
-                        if (n - j) % 2 == 0 {
-                            print!(".")
-                        } else {
-                            print!("#")
-                        }
-                    } else {
-                        if i % 2 == 0 {
-                            print!("#")
-                        } else {
-                            print!(".")
-                        }
-                    }
-                }
-            } else {
-                // bottom half
-                if j < m {
-                    // left half
-                    if j < n - i {
-                        // outer
-                        if j % 2 == 0 {
-                            print!("#")
-                        } else {
-                            print!(".")
-                        }
-                    } else {
-                        // inner
-                        if (n - i) % 2 == 0 {
-                            print!(".")
-                        } else {
-                            print!("#")
-                        }
-                    }
-                } else {
-                    // right half
-                    if i <= j {
-                        // outer
-                        if (n - j) % 2 == 0 {
-                            print!(".")
-                        } else {
-                            print!("#")
-                        }
-                    } else {
-                        if (n - i) % 2 == 0 {
-                            print!(".")
-                        } else {
-                            print!("#")
-                        }
-                    }
-                }
-            }
-        }
-        println!();
-    }
+    s.sort_by(|a, b| a.len().cmp(&b.len()));
+
+    println!("{}", s.join(""));
 }
