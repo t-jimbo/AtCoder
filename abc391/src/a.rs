909ffd72167377b47cce4d51ce211a1b620b130c
t-jimbo <jbear.um@gmail.com>
2025-03-09 13:17:48 +0900

    a

--- abc391/src/a.rs
+++ abc391/src/a.rs
@@ -1,29 +1,23 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
+
+fn rev(c: char) -> char {
+    match c {
+        'N' => 'S',
+        'S' => 'N',
+        'E' => 'W',
+        'W' => 'E',
+        _ => c,
+    }
+}
 
 fn main() {
     input! {
-        n: usize,
-        a: [usize; n],
+        d: Chars,
     }
 
-    let mut count = 0;
-    for i in 0..n {
-        if i == 0 {
-            count += 1;
-            continue;
-        }
-
-        if a[i] == a[i - 1] {
-            count += 1;
-        } else {
-            count = 1;
-        }
-
-        if count >= 3 {
-            println!("Yes");
-            return;
-        }
+    for c in d {
+        print!("{}", rev(c));
     }
 
-    println!("No");
+    println!();
 }
